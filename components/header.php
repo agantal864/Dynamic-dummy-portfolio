209ceaaa93e0d5106b0e16b6918c489a751f8f77
Azis Agantal <aamagantal@gmail.com>
2020-05-28 23:10:01 +0800

    add to my web

--- components/header.php
+++ components/header.php
@@ -36,7 +36,7 @@
         <div class="collapse navbar-collapse" id="nav-menu">
           <ul id="color-active" class="navbar-nav ml-auto navbar-brand animated fadeIn delay-1s">
               <li class="nav-item"><a class="nav-link" href="index.php" id="main-home"><span class="nav-hover-effect">Home</span></a></li>
-              <li class="nav-item"><a data-scroll class="nav-link" href="index.php #hrefprof"><span class="nav-hover-effect">Profile</span></a></li>
+              <li class="nav-item"><a data-scroll class="nav-link" href="#hrefprof"><span class="nav-hover-effect">Profile</span></a></li>
               <li class="nav-item"><a class="nav-link" href="blog.php"><span class="nav-hover-effect">Blog</span></a></li>
               <li class="nav-item"><a class="nav-link" href="myfile/myresume.pdf"><span class="nav-hover-effect">Resume</span></a></li>
               <li class="nav-item"><a class="nav-link" href="contacts.php"><span class="nav-hover-effect">Contact</span></a></li>
--- config/db_connect.php
+++ config/db_connect.php
@@ -1,8 +1,12 @@
 <?php
-      $connect = mysqli_connect('localhost', 'agantal864', 'Azis62017050105807%', 'contacts');
-
+      $connect = mysqli_connect('localhost', 'aamagantal', 'Azis62017050105807%', 'myweb_db');
       if (!$connect) {
         echo 'Connection error'. mysqli_connect_error();
       }
-      
+
+      if(mysqli_ping($connect)) {
+        echo "connection ok!";
+      } else {
+        echo "error: " . mysqli_error($connect);
+      }
  ?>
